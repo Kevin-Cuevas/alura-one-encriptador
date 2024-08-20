073c40d0cb426d1229c3ae7565965d4f90d2ab07
Kevin-Cuevas <nivekcuevas.dev@gmail.com>
2024-08-19 21:36:00 -0600

    feat: add logic for encrypt text

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+// * Reglas de encriptacion:
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+// * Functions
+const encrypt = function (text) {
+  let result = "";
+
+  for (const word of text) {
+    switch (word) {
+      case "a":
+        result += "ai";
+        break;
+      case "e":
+        result += "enter";
+        break;
+      case "i":
+        result += "imes";
+        break;
+      case "o":
+        result += "ober";
+        break;
+      case "u":
+        result += "ufat";
+        break;
+      default:
+        result += word;
+        break;
+    }
+  }
+
+  console.log(result);
+};
+
+encrypt(`Hola como estas`); // Hoberlai cobermober enterstais
